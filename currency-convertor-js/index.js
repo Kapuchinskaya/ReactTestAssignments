0d3b69a89b41131090dad9c363bb50c5103ab9cf
VolhaKuchynskaya <kapuchinskaya.study@gmail.com>
2021-10-08 15:39:52 +0200

    feat: currency convertor with immediate rates recalculation

---
+++ currency-convertor-js/index.js
@@ -0,0 +1,314 @@
+/*TODO
+delete button
+change state structure (from Arrays to Objects)
+data formatter and alerts (if not numbers || <0 are entered in Input)
+rates - from API
+*/
+
+//все rates - к USD
+const conversionRates = {
+  USD: 1,
+  // AED: 3.6725,
+  // AFN: 83.684,
+  // ALL: 104.3968,
+  // AMD: 487.6631,
+  // ANG: 1.79,
+  // AOA: 601.4619,
+  // ARS: 98.4678,
+  // AUD: 1.3777,
+  // AWG: 1.79,
+  // AZN: 1.6941,
+  // BAM: 1.6836,
+  // BBD: 2,
+  // BDT: 85.4196,
+  // BGN: 1.6838,
+  // BHD: 0.376,
+  // BIF: 1980.354,
+  // BMD: 1,
+  // BND: 1.3549,
+  // BOB: 6.8804,
+  // BRL: 5.4253,
+  // BSD: 1,
+  // BTN: 74.1511,
+  // BWP: 11.3085,
+  BYN: 2.505,
+  // BZD: 2,
+  // CAD: 1.2645,
+  // CDF: 1979.4986,
+  // CHF: 0.9303,
+  // CLP: 810.3446,
+  // CNY: 6.4511,
+  // COP: 3819.5944,
+  // CRC: 624.0675,
+  // CUC: 1,
+  // CUP: 25,
+  // CVE: 94.9172,
+  // CZK: 21.8731,
+  // DJF: 177.721,
+  // DKK: 6.422,
+  // DOP: 56.2137,
+  // DZD: 136.7385,
+  // EGP: 15.6803,
+  // ERN: 15,
+  // ETB: 46.2037,
+  EUR: 0.8609,
+  // FJD: 2.1016,
+  // FKP: 0.738,
+  // FOK: 6.422,
+  GBP: 0.738,
+  // GEL: 3.1136,
+  // GGP: 0.738,
+  // GHS: 6.003,
+  // GIP: 0.738,
+  // GMD: 52.1773,
+  // GNF: 9717.5384,
+  // GTQ: 7.719,
+  // GYD: 208.3134,
+  // HKD: 7.7837,
+  // HNL: 24.0347,
+  // HRK: 6.4858,
+  // HTG: 97.5934,
+  // HUF: 309.8181,
+  // IDR: 14257.1819,
+  // ILS: 3.2365,
+  // IMP: 0.738,
+  // INR: 74.1514,
+  // IQD: 1455.1281,
+  // IRR: 41723.4243,
+  // ISK: 129.9556,
+  // JMD: 147.6708,
+  // JOD: 0.709,
+  // JPY: 111.1172,
+  // KES: 110.2743,
+  // KGS: 84.5964,
+  // KHR: 4060.6141,
+  // KID: 1.3776,
+  // KMF: 423.4907,
+  // KRW: 1183.1282,
+  // KWD: 0.2996,
+  // KYD: 0.8333,
+  // KZT: 425.3891,
+  // LAK: 9852.6396,
+  // LBP: 1507.5,
+  // LKR: 199.2008,
+  // LRD: 170.5477,
+  // LSL: 14.9024,
+  // LYD: 4.5237,
+  // MAD: 9.0266,
+  // MDL: 17.5762,
+  // MGA: 3963.7683,
+  // MKD: 52.9093,
+  // MMK: 1865.38,
+  // MNT: 2845.3512,
+  // MOP: 8.0172,
+  // MRU: 36.0532,
+  // MUR: 42.4292,
+  // MVR: 15.3937,
+  // MWK: 813.4451,
+  // MXN: 20.5108,
+  // MYR: 4.1799,
+  // MZN: 63.94,
+  // NAD: 14.9024,
+  // NGN: 421.7599,
+  // NIO: 35.0256,
+  // NOK: 8.6502,
+  // NPR: 118.6418,
+  // NZD: 1.4428,
+  // OMR: 0.3845,
+  // PAB: 1,
+  // PEN: 4.1249,
+  // PGK: 3.5214,
+  // PHP: 50.755,
+  // PKR: 170.4403,
+  PLN: 3.9554,
+  // PYG: 6853.991,
+  // QAR: 3.64,
+  // RON: 4.2617,
+  // RSD: 100.9878,
+  RUB: 72.8855,
+  // RWF: 1014.116,
+  // SAR: 3.75,
+  // SBD: 7.9496,
+  // SCR: 13.1561,
+  // SDG: 439.4487,
+  // SEK: 8.7535,
+  // SGD: 1.3549,
+  // SHP: 0.738,
+  // SLL: 10546.7683,
+  // SOS: 576.4521,
+  // SRD: 21.3555,
+  // SSP: 177.444,
+  // STN: 21.0898,
+  // SYP: 1677.7564,
+  // SZL: 14.9024,
+  // THB: 33.6743,
+  // TJS: 11.2812,
+  // TMT: 3.4903,
+  // TND: 2.8119,
+  // TOP: 2.2631,
+  // TRY: 8.8624,
+  // TTD: 6.7623,
+  // TVD: 1.3776,
+  // TWD: 27.7705,
+  // TZS: 2300.1177,
+  // UAH: 26.5974,
+  // UGX: 3539.9365,
+  // UYU: 42.6902,
+  // UZS: 10702.378,
+  // VES: 2402440.028,
+  // VND: 22686.8798,
+  // VUV: 112.1412,
+  // WST: 2.579,
+  // XAF: 564.6543,
+  // XCD: 2.7,
+  // XDR: 0.7098,
+  // XOF: 564.6543,
+  // XPF: 102.7222,
+  // YER: 249.5301,
+  // ZAR: 14.9023,
+  // ZMW: 16.6745,
+};
+
+const state = {
+  currencies: ["USD", "USD"],
+  amounts: [0, 0],
+};
+
+//NODE ElEMENTS
+const selects = document.querySelectorAll(".select-box-currency");
+const getAllSelects = () => document.querySelectorAll(".select-box-currency");
+const currencyContainers = document.querySelectorAll(".one-currency-container");
+const getCurrencyContainers = () =>
+  document.querySelectorAll(".one-currency-container");
+
+const btnAdd = document.querySelector(".add-button");
+
+//EVENT LISTENERS
+//function to recalculate rates
+const rateCalculator = (id, value, changedField) => {
+  if (changedField === "amount-box") {
+    const newAmounts = state.amounts.map((el, index) => {
+      if (index == id) {
+        return value;
+      }
+
+      const firstRate = conversionRates[state.currencies[id]];
+      const secondRate = conversionRates[state.currencies[index]];
+
+      if (!firstRate || !secondRate) {
+        return 0;
+      }
+
+      return ((value * secondRate) / firstRate).toFixed(2);
+    });
+    state.amounts = newAmounts;
+  }
+
+  if (changedField === "select-box-currency") {
+    state.currencies[id] = value;
+    const newAmounts = state.amounts.map((el, index) => {
+      if (index == id) {
+        return el;
+      }
+
+      const firstRate = conversionRates[state.currencies[id]];
+      const secondRate = conversionRates[state.currencies[index]];
+
+      if (!firstRate || !secondRate) {
+        return 0;
+      }
+
+      return ((state.amounts[id] * secondRate) / firstRate).toFixed(2);
+    });
+    state.amounts = newAmounts;
+  }
+};
+
+//listen to the changes in input and in select fields
+const currencyContainersEventListener = (event) => {
+  switch (event.target.className) {
+    case "amount-box":
+      rateCalculator(event.currentTarget.id, event.target.value, "amount-box");
+      break;
+    case "select-box-currency":
+      rateCalculator(
+        event.currentTarget.id,
+        event.target.value,
+        "select-box-currency"
+      );
+      break;
+    default:
+      break;
+  }
+
+  updateAmounts();
+};
+
+//currencyContainers
+currencyContainers.forEach((item) => {
+  item.addEventListener("change", currencyContainersEventListener);
+});
+
+currencyContainers.forEach((item) => {
+  item.addEventListener("keyup", currencyContainersEventListener);
+});
+
+const updateAmounts = () => {
+  getCurrencyContainers().forEach((container) => {
+    const amountBox = container.querySelector(".amount-box");
+    amountBox.value = state.amounts[container.id];
+  });
+};
+
+//ADD BUTTON
+btnAdd.addEventListener("click", () => addCurrency());
+
+//OPTIONS LIST
+const createOptions = () => {
+  const options = Object.keys(conversionRates);
+  selects.forEach((select) => {
+    //add options
+    options.forEach((key) => {
+      const opt = document.createElement("option");
+      opt.value = key;
+      opt.innerHTML = key;
+      select.appendChild(opt);
+    });
+  });
+};
+
+//FUNCTION TO ADD NEW CURRENCY
+const addCurrency = () => {
+  //choose element and clone
+  const itemToClone = document.querySelector(
+    ".currencies-container"
+  ).lastElementChild;
+
+  const newItem = itemToClone.cloneNode(true);
+
+  //add event listeners to copy
+  newItem.addEventListener("keyup", currencyContainersEventListener);
+  newItem.addEventListener("change", currencyContainersEventListener);
+
+  //set id for new currency container
+  const newIndexGenerator = () => {
+    const newIndex = +itemToClone.id + 1;
+    return {
+      newIndex: newIndex,
+    };
+  };
+  const newNameData = newIndexGenerator();
+  newItem.id = newNameData.newIndex;
+
+  //set init values
+  newItem.querySelector(".select-box-currency").value = state.currencies[0];
+  newItem.querySelector(".amount-box").value = 0;
+
+  state.amounts[newNameData.newIndex] = 0;
+  state.currencies[newNameData.newIndex] = state.currencies[0];
+
+  //insert element
+  itemToClone.after(newItem);
+};
+
+createOptions();
